9c1013770da025750dd7ef0c67c9f508471b93d0
Denis <Denis@MSI>
2021-09-20 17:18:15 +0300

    Добавьте файлы проекта.

---
+++ SolutionForGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SolutionForGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
